5b3e6ab072700c4e42aa2cee1fee807de96aaeb6
Maahhiii <mahikachhadiya007@gmail.com>
2025-04-02 00:03:38 +0530

    new changes

--- PInfluence/src/App.jsx
+++ PInfluence/src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
+import PinterestLayout from './components/PinterestLayout.jsx';
+import './styles.css';
 
 function App() {
-  return <HomePage />;
-}
 
-export default App;
+  return (
+		<div>
+			<PinterestLayout />
+		</div>
+	);
+}
