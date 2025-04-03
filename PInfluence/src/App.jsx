eaa8872e9d32e6c0c99213ee6d5eae666958d0f7
Maahhiii <mahikachhadiya007@gmail.com>
2025-04-03 11:46:47 +0530

    pulled from main

--- PInfluence/src/App.jsx
+++ PInfluence/src/App.jsx
@@ -4,45 +4,53 @@ import Grid from './Grid';
 import Navbar from './Navbar';
 import './App.css';
 
-function App() {
-  const [cards, setCards] = useState([]);
+// function App() {
+//   const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    const fetchImages = () => {
-      // Add more product information
-      const images = [
-        { 
-          image: '/images/image1.jpg', 
-          title: 'Pink Oversized Coat',
-          brand: 'Fashion Nova',
-          shopLink: 'https://example.com/pink-coat'
-        },
-        { 
-          image: '/images/image2.jpg', 
-          title: 'White Knit Dress',
-          brand: 'Zara',
-          shopLink: 'https://example.com/white-dress'
-        },
-        { 
-          image: '/images/image3.jpg', 
-          title: 'Argyle Vest with Pants',
-          brand: 'H&M',
-          shopLink: 'https://example.com/argyle-outfit'
-        },
-      ];
+//   useEffect(() => {
+//     const fetchImages = () => {
+//       // Add more product information
+//       const images = [
+//         { 
+//           image: '/images/image1.jpg', 
+//           title: 'Pink Oversized Coat',
+//           brand: 'Fashion Nova',
+//           shopLink: 'https://example.com/pink-coat'
+//         },
+//         { 
+//           image: '/images/image2.jpg', 
+//           title: 'White Knit Dress',
+//           brand: 'Zara',
+//           shopLink: 'https://example.com/white-dress'
+//         },
+//         { 
+//           image: '/images/image3.jpg', 
+//           title: 'Argyle Vest with Pants',
+//           brand: 'H&M',
+//           shopLink: 'https://example.com/argyle-outfit'
+//         },
+//       ];
     
-      const repeatedImages = Array(20).fill(images).flat();
-      setCards(repeatedImages);
-    };
+//       const repeatedImages = Array(20).fill(images).flat();
+//       setCards(repeatedImages);
+//     };
 
-    fetchImages();
-  }, []);
+//     fetchImages();
+//   }, []);
 
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <Grid cards={cards} />
+//     </div>
+//   );
+// }
+
+function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Grid cards={cards} />
-    </div>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+    </h1>
   );
 }
 
