07f8cfa7921ab55ba01b1553af5c7877130e0aa1
Maahhiii <158501976+Maahhiii@users.noreply.github.com>
2025-07-02 11:11:12 +0530

    Update FirebaseConfig.js

--- PInfluence/src/FirebaseConfig.js
+++ PInfluence/src/FirebaseConfig.js
@@ -7,13 +7,7 @@ import { GoogleAuthProvider } from "firebase/auth";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBN-qdwgE6cVbv46gNj_trAlWcOmJ1w4dY",
-  authDomain: "pinfluence-f7ed1.firebaseapp.com",
-  projectId: "pinfluence-f7ed1",
-  storageBucket: "pinfluence-f7ed1.firebasestorage.app",
-  messagingSenderId: "1079964332527",
-  appId: "1:1079964332527:web:f7ca4f5dcb7e40e3e4c61a",
-  measurementId: "G-NTEMBRDCBV"
+  //add your config here
 };
 
 // Initialize Firebase
